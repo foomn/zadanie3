52d86610c86605ee30776569a268917c88d84a6a
foomn <103070159+foomn@users.noreply.github.com>
2025-04-09 14:20:41 +0300

    Delete 11.py

--- 11.py
+++
@@ -1,9 +0,0 @@
-while True:
-    expression = input("Введите арифметическое выражение (или 'exit' для выхода): ")
-    if expression.lower() == "exit":
-        break
-    try:
-        result = eval(expression)
-        print(f"Результат: {result}")
-    except Exception as e:
-        print(f"Ошибка: {e}")
